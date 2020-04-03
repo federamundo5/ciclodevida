1f4cc59ceb3198bdda42637e7a02b61e858e5030
Federico Ramundo <federicoramundo5@gmail.com>
2020-04-03 12:36:14 -0300

    test

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static(__dirname + '/index'));
+app.use(express.static(__dirname + '/angularapp'));
 app.listen(process.env.PORT || 8080);
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });
 console.log('Finalizo!!');
