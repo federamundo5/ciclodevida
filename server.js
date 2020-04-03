ea0049a10930eaab4c4012110ab0b23cfc879811
Federico Ramundo <federicoramundo5@gmail.com>
2020-04-03 15:04:28 -0300

    wo

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const path = require('path');
 app.use(express.static(__dirname + '/angularapp'))
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
-    res.sendfile('./index.html');
+    res.sendfile('../index.html');
 });
 console.log('Build successful!!');
