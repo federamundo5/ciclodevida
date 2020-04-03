3e1b32c5efd92ba5cf072e0dab087b30f2e09902
Federico Ramundo <federicoramundo5@gmail.com>
2020-04-03 12:20:50 -0300

    commit 4

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static(__dirname + '/angularapp'));
+app.use(express.static(__dirname + '/index'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/my-dream-app/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 console.log('Finalizo!!');
